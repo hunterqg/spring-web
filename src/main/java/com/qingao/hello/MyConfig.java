5fa424c127d3d567a709757c229aed21c9bd1c42
qingao <qingao@cmcm.com>
2016-07-21 15:24:14 +0800

    添加了使用外部properties文件

--- src/main/java/com/qingao/hello/MyConfig.java
+++ src/main/java/com/qingao/hello/MyConfig.java
@@ -1,13 +1,20 @@
 package com.qingao.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.core.env.Environment;
 
 import java.util.ArrayList;
 
 @Configuration
+@PropertySource("classpath:env.properties")
 public class MyConfig {
     @Bean(name = "dataList")
+    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
     public ArrayList<String> getDataList() {
         ArrayList<String> list = new ArrayList<String>();
         list.add("one");
@@ -16,6 +23,10 @@ public class MyConfig {
         list.add("four");
         list.add("five");
         list.add("six");
+        list.add(env.getProperty("key_seven"));
         return list;
     }
+
+    @Autowired
+    Environment env;
 }
